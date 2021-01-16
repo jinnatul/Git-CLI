7ae89904be15746247aeb6b36dcd40d8f7ab39cd
jinnatul <morolswediu@gmail.com>
2021-01-16 13:42:12 +0600

    create a remote repository command

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ import {
   getCurrentDirectoryBase,
 } from './lib/files';
 import {
+  githubAuth,
   getStoredGithubToken,
   setGithubCredentials,
   registerNewToken,
 } from './lib/githubCredentials';
+import {
+
+} from './lib/inquirer';
+import {
+  createRemoteRepository,
+  createGitIgnore,
+  setupRepository,
+} from './lib/createRepo';
 
 myApp
   .command('init')
@@ -31,6 +40,46 @@ myApp
       token = await registerNewToken();
     }
   });
+
+myApp
+  .command('create_repo')
+  .description('Create a new repository on Github') 
+  .action(async () => {
+    const getGithubToken = async () => {
+      let token = getStoredGithubToken();
+      if (token) {
+        return token;
+      }
+      await setGithubCredentials();
+      token = await registerNewToken();
+      return token;
+    }
+
+    try {
+      const token = await getGithubToken();
+      githubAuth(token);
+
+      const url = await createRemoteRepository();
+      await createGitIgnore();
+
+      const complete = await setupRepository(url);
+      if (complete) {
+        console.log(chalk.green('All done!'));
+      }
+    } catch (error) {
+      switch (error.status) {
+        case 401:
+          console.log(chalk.red('Could\'t log you in. Please provide correct credentials or token.'));
+          break;
+        case 422:
+          console.log(chalk.red('There already exists a remote repository with the same name.'));
+          break;
+        default:
+          console.log(error);
+          break;
+      }
+    }
+  }) 
 
 myApp.parse(process.argv);
 
